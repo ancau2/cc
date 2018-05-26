2602cee1c4ed9a2e0e1d4d671786248a0ae7905a
Adrian <adrian@boemeto.com>
2018-05-26 16:11:58 +0300

    cc6

--- CC3.c
+++ CC3.c
@@ -9,8 +9,8 @@ S = {0,1}
 g:
      0       1        Eps
 q1  {q1}   {q1,q2}     {}
-q2  {q3}     {}        {q3}
-q3  {}       {q4}      {}
+q2  {q3}     {}        {}
+q3  {}       {q4}      {q3}
 q4  {q4}     {q4}      {}
 
 q1 -> start state
@@ -18,41 +18,72 @@ q1 -> start state
 F = {q4}
 */
 
-int accepted(const char* string){
-	int state = 1;
-	int n = 0;
-	while(n < strlen(string)){
-		if(state == 1){
+int accepted(int state,int n,const char* string){
+    while(n < strlen(string)){
+        printf("stare: %d caracter: %c\n",state,string[n]);
+        switch(state){
+            case 1:
+                if (string[n] == '0') {
+                    n++;
+                } else if (string[n] == '1') {
+                    n++;
+                    if (accepted(1, n, string) == 0) {
+                        printf("traseu incorect, mergem in starea 2\n");
+                        state = 2;
+                    }
+                    else{
+                        return 1;
+                    }
+                }else{
+                    return 0;
+                }
+                break;
+            case 2:
+                if(string[n]!='1'){
+                    state=3;
+                    n++;
+                }
+                else{
+                    return 0;
+                }
+                break;
+            case 3:
+                if(string[n]=='1'){
+                    state=4;
+                    n++;
+                }
+                else {
+                    return 0;
+                }
+                break;
+            case 4:
+                if(string[n]=='1'||string[n]=='1'){
+                    n++;
+                }
+                else{return 0;}
+                break;
+            default:
+                printf("nu a intrat pe nicio stare");
+                break;
+        }
+    }
 
-		}
-
-		if(state == 2){
-		
-		}
-
-		if(state == 3){
-
-		}
-
-		if(state == 4){
-			
-		}
-	}
-
-	if((n == strlen(string)) && (state == 4))
-		return 1;
-    else return 0;
+    if (state == 4) {
+        return n == strlen(string) ? 1 : 0;
+    } else {
+        return 0;
+    }
 }
 
 int main(int argc, char const *argv[])
 {
-	if(argc < 2){
-		printf("Se asteapta introducerea unui parametru\n");
-		exit(1);
-	}
-	if(accepted(argv[1]))
-		printf("String acceptat\n");
-	else 
-		printf("String refuzat\n");
-	return 0;
+    if(argc < 2){
+        printf("Se asteapta introducerea unui parametru\n");
+        exit(1);
+    }
+    if(accepted(1,0,argv[1]))
+        printf("String acceptat\n");
+    else
+        printf("String refuzat\n");
+    return 0;
 }
